2cf6eaf3a57143223225da379d66f2d71a2b55f7
JGardeskiIV <jj@gardeski.com>
2015-09-23 21:59:13 -0700

    Apache POI Setup & basic test.

--- src/com/CC2UC/Main.java
+++ src/com/CC2UC/Main.java
@@ -1,11 +1,19 @@
 package com.CC2UC;
 
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.openxml4j.opc.OPCPackage;
+
 public class Main {
 
-    private static final String WORD_LIST = "Vocab Sample.xlsx";
-    private static final String WORD_LIST_QUOTES = "\"Vocab Sample.xlsx\"";
+    private static final String WORD_LIST = "Sample.xlsx";
+    private static final String WORD_LIST_QUOTES = "\"Sample.xlsx\"";
 
     public static void main(String[] args) {
-
+        try {
+            OPCPackage pkg = OPCPackage.open("Sample.xlsx");
+            System.out.println("Opened Successfully.");
+        } catch (InvalidFormatException e) {
+            System.out.println("Failed to Open.");
+        }
     }
 }
