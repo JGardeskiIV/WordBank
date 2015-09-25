e4818e7456d63202baf299a4bd7fffcade61c403
JGardeskiIV <jj@gardeski.com>
2015-09-24 22:21:25 -0700

    Continuing Apache POI XSSF testing.

--- src/com/CC2UC/Main.java
+++ src/com/CC2UC/Main.java
@@ -1,7 +1,15 @@
 package com.CC2UC;
 
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.openxml4j.exceptions.OpenXML4JException;
 import org.apache.poi.openxml4j.opc.OPCPackage;
+import org.apache.poi.xssf.eventusermodel.XSSFReader;
+import org.apache.poi.xssf.model.SharedStringsTable;
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
+import org.xml.sax.helpers.XMLReaderFactory;
+
+import java.io.IOException;
 
 public class Main {
 
@@ -10,10 +18,19 @@ public class Main {
 
     public static void main(String[] args) {
         try {
-            OPCPackage pkg = OPCPackage.open("Sample.xlsx");
+            OPCPackage pkg = OPCPackage.open( WORD_LIST );
             System.out.println("Opened Successfully.");
+
+            XSSFReader reader = new XSSFReader( pkg );
+            System.out.println("XSSFReader Established.");
+
+            
         } catch (InvalidFormatException e) {
             System.out.println("Failed to Open.");
+        } catch (OpenXML4JException e2) {
+            System.out.println("OpenXML4JException with XSSFReader.");
+        } catch (IOException e3) {
+            System.out.println("IOException with XSSFReader.");
         }
     }
 }
